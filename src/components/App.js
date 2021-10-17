97ff157d1a4e442961421d89fd4426606e011935
Praveen Kumar <praveenscience@gmail.com>
2021-10-17 09:28:45 +0100

    Implemented NavBar.

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,19 @@
 import React, { Component } from "react";
+import Navbar from "./Bootstrap/Navbar";
 
 class App extends Component {
   state = {
-    Name: "",
-    Refresh: false
+    Dark: false
   };
-  componentDidMount() {
-    console.log("componentDidMount called!");
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate called!");
-    console.log({ prevProps, prevState });
-    if (prevState.Refresh !== this.state.Refresh) {
-      window.alert("Call the API!");
-    }
-  }
+  toggleNav = () => {
+    this.setState({ Dark: !this.state.Dark });
+  };
   render() {
-    console.log("render called!");
     return (
       <div className="App">
-        <p>
-          Hello.{" "}
-          {this.state.Name.trim().length === 0
-            ? "Please Enter your Name!"
-            : `My name is ${this.state.Name}!`}
-        </p>
-        <input
-          type="text"
-          value={this.state.Name}
-          onChange={e => {
-            this.setState({ Name: e.target.value });
-          }}
-        />
-        <br />
-        <input
-          type="text"
-          value={this.state.Name}
-          onChange={e => {
-            this.setState({ Name: e.target.value });
-          }}
-        />
-        <br />
-        <button
-          onClick={() => {
-            this.setState({ Refresh: !this.state.Refresh });
-          }}
-          disabled={this.state.Name.trim().length === 0}
-        >
-          Refresh
-        </button>
+        <Navbar dark={this.state.Dark} toggleNav={this.toggleNav}>
+          React Application
+        </Navbar>
       </div>
     );
   }
---
+++ src/components/Bootstrap/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Navbar = ({ children, dark, toggleNav }) => {
+  dark = !!dark ? "dark" : "light";
+  return (
+    <nav className={`navbar navbar-expand navbar-${dark} bg-${dark}`}>
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">{children}</span>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <span className="nav-link nav-action" onClick={toggleNav}>
+                Toggle Nav
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
