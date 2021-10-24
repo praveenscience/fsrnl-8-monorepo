058b92815910f8a3197c277d4f2122eabc6b1531
Praveen Kumar <praveenscience@gmail.com>
2021-10-24 08:08:42 +0100

    Created a basic state for Form fields.

--- src/components/_FormsFunc.js
+++ src/components/_FormsFunc.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
 const FormsFunc = () => {
+  const [Forms, setForms] = useState({
+    Username: "",
+    FullName: "",
+    Password: "",
+    ConfPass: ""
+  });
   return <form action="">Form using Stateless Functional Component</form>;
 };
 
