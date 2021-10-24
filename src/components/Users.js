0a8b933746392e9f5de973b11dc5f79c0bc85f4d
Praveen Kumar <praveenscience@gmail.com>
2021-10-24 07:10:06 +0100

    Fixed the margins for Users.

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,7 +4,7 @@ const Users = ({ users }) => {
   return (
     <div className="row">
       {users.map((user, key) => (
-        <div className="col-12 col-md-6 col-lg-4 col-xl-3" key={key}>
+        <div className="col-12 col-md-6 col-lg-4 col-xl-3 mb-3" key={key}>
           <User user={user} />
         </div>
       ))}
