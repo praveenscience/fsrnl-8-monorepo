34c3999c3832cfdfdeebacabb342fd8dfd0e628e
Praveen Kumar <praveenscience@gmail.com>
2021-10-24 08:05:31 +0100

    Added keys and mapper.

--- src/components/_FormsState.js
+++ src/components/_FormsState.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import FormGroup from "./Bootstrap/FormGroup";
 
 class FormsState extends Component {
+  state = {
+    Username: "",
+    FullName: "",
+    Password: "",
+    ConfPass: ""
+  };
   render() {
-    return <form>Form using Stateful Class Component</form>;
+    const Mapper = {
+      Username: ["text", "Username"],
+      FullName: ["text", "FullName"],
+      Password: ["password", "Password"],
+      ConfPass: ["password", "Confirm Password"]
+    };
+    return (
+      <form>
+        {Object.keys(this.state).map(key => (
+          <FormGroup
+            key={key}
+            Label={Mapper[key][1]}
+            Id={key}
+            Type={Mapper[key][0]}
+            Desc={"Please enter some value for " + Mapper[key][1]}
+          />
+        ))}
+      </form>
+    );
   }
 }
 
