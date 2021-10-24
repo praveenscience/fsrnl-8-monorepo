1c50c57b0dc6933054e535ef2dbff04988752517
Praveen Kumar <praveenscience@gmail.com>
2021-10-24 08:08:58 +0100

    Added the mapper to SFC.

--- src/components/_FormsFunc.js
+++ src/components/_FormsFunc.js
@@ -7,6 +7,12 @@ const FormsFunc = () => {
     Password: "",
     ConfPass: ""
   });
+  const Mapper = {
+    Username: ["text", "Username"],
+    FullName: ["text", "FullName"],
+    Password: ["password", "Password"],
+    ConfPass: ["password", "Confirm Password"]
+  };
   return <form action="">Form using Stateless Functional Component</form>;
 };
 
