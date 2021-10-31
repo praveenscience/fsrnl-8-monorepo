341cb7a759720221c2e2be2e523af142bfe5ecb4
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 08:48:11 +0000

    Implemented route for root.

--- server/routes/root.js
+++ server/routes/root.js
@@ -3,5 +3,28 @@ const express = require("express");
 // Create an instance of router.
 const app = express.Router();
 
+// Create a GET Path.
+app.get("/", (req, res) => {
+  let output = "Hello Geeks, Welcome to HomePage!";
+  if (Object.values(req.query).length > 0) {
+    output += " Here's some queries: " + JSON.stringify(req.query, null, 2);
+  }
+  res.send(output);
+});
+// Dyanmic Parameter Routing.
+app.get("/:user", (req, res) => {
+  res.send(`Hello Geeks, Welcome to ${req.params.user}'s HomePage!`);
+});
+app.post("/", (req, res) => {
+  let output = "Hello Advanced Geeks!";
+  if (Object.values(req.query).length > 0) {
+    output += " Here's some queries: " + JSON.stringify(req.query, null, 2);
+  }
+  if (Object.values(req.body).length > 0) {
+    output += " Here's some body: " + JSON.stringify(req.body, null, 2);
+  }
+  res.send(output);
+});
+
 // Export the app.
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -6,33 +6,15 @@ const express = require("express");
 const app = express();
 // Define a port address.
 const port = 5000;
+// Import routes.
+const root = require("./routes/root");
 
 // Use a Middleware to parse POST Data.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Create a GET Path.
-app.get("/", (req, res) => {
-  let output = "Hello Geeks, Welcome to HomePage!";
-  if (Object.values(req.query).length > 0) {
-    output += " Here's some queries: " + JSON.stringify(req.query, null, 2);
-  }
-  res.send(output);
-});
-// Dyanmic Parameter Routing.
-app.get("/:user", (req, res) => {
-  res.send(`Hello Geeks, Welcome to ${req.params.user}'s HomePage!`);
-});
-app.post("/", (req, res) => {
-  let output = "Hello Advanced Geeks!";
-  if (Object.values(req.query).length > 0) {
-    output += " Here's some queries: " + JSON.stringify(req.query, null, 2);
-  }
-  if (Object.values(req.body).length > 0) {
-    output += " Here's some body: " + JSON.stringify(req.body, null, 2);
-  }
-  res.send(output);
-});
+// Require modular routes.
+app.use("/", root);
 
 // Listen the app on port.
 app.listen(port, () => {
