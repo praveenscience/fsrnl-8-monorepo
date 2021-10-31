230122a4baee18d2b7530cc2293f666f2383ef12
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 07:32:00 +0000

    Created a basic Express JS Web server.

--- server/server.js
+++ server/server.js
@@ -4,3 +4,10 @@ const express = require("express");
 // Initialise a server side app.
 // Express app = new Express(); (Java)
 const app = express();
+// Define a port address.
+const port = 5000;
+
+// Listen the app on port.
+app.listen(port, () => {
+  console.log(`Server started in port ${port}.`);
+});
