81010262bfd843d277c23fa2079a20950d4cbb06
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 09:18:58 +0000

    Attached the Users List API Endpoint.

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,10 +2,13 @@
 const express = require("express");
 // Create an instance of router.
 const app = express.Router();
+// Import routes.
+const users = require("./api/users");
 
 app.get("/", (req, res) => {
   res.json("Welcome to API Server.");
 });
+app.use("/users", users);
 
 // Export the app.
 module.exports = app;
