27f4f6b044bbe9b9d8e96217dcfcf5a7c437ff04
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 07:36:08 +0000

    Created a GET & POST endpoints for the root route.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ const app = express();
 // Define a port address.
 const port = 5000;
 
+// Create a GET Path.
+app.get("/", (req, res) => {
+  res.send("Hello Geeks!");
+});
+app.post("/", (req, res) => {
+  res.send("Hello Advanced Geeks!");
+});
+
 // Listen the app on port.
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
