663eed509e883aa7d2da3b46c64715e01937acfc
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 08:44:47 +0000

    Implemented Dyanmic Parameter Routing.

--- server/server.js
+++ server/server.js
@@ -13,11 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 // Create a GET Path.
 app.get("/", (req, res) => {
-  let output = "Hello Geeks!";
+  let output = "Hello Geeks, Welcome to HomePage!";
   if (Object.values(req.query).length > 0) {
     output += " Here's some queries: " + JSON.stringify(req.query, null, 2);
   }
   res.send(output);
+});
+// Dyanmic Parameter Routing.
+app.get("/:user", (req, res) => {
+  res.send(`Hello Geeks, Welcome to ${req.params.user}'s HomePage!`);
 });
 app.post("/", (req, res) => {
   let output = "Hello Advanced Geeks!";
