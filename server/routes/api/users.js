677f70e4a1f6970b8f63e3ed73931ff1385eace0
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 09:20:14 +0000

    Implemented single user request.

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -5,8 +5,13 @@ const app = express.Router();
 // Import the Users.
 const Users = require("../../constants/users.json");
 
+// R from CRUD: List All
 app.get("/", (req, res) => {
   res.json(Users);
+});
+// R from CRUD: List One
+app.get("/:Username", (req, res) => {
+  res.json(Users.find(user => user.Username === req.params.Username));
 });
 
 // Export the app.
