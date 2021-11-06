cd2c52e56b38aa4e3e1782208f7ecadee5833583
Praveen Kumar <praveenscience@gmail.com>
2021-11-06 08:03:46 +0000

    Added a list endpoint.

--- server/routes/api.js
+++ server/routes/api.js
@@ -68,6 +68,23 @@ app.get("/db/:action", (req, res) => {
         }
       });
       break;
+    case "list":
+      // Check and return the status.
+      db.schema.hasTable("gfg_fbclone_users").then(exists => {
+        if (exists) {
+          // Table exists, do nothing.
+          console.log("Table Exists");
+          db("gfg_fbclone_users").then(rows => {
+            res.json(rows);
+          });
+        } else {
+          // Table not there.
+          console.log("Table Doesn't Exist");
+          // Create the table.
+          res.status(404).json("Table Doesn't Exist.");
+        }
+      });
+      break;
   }
 });
 app.use("/users", users);
