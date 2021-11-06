ac7d6967d29f93b84d14dfd412c9b6efc8919de1
Praveen Kumar <praveenscience@gmail.com>
2021-11-06 07:40:08 +0000

    Created an endpoint for table.
    * Check if table exists.
    * If exists, do nothing.
    * If doesn't, create a table with default schema.

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,6 +8,32 @@ const users = require("./api/users");
 app.get("/", (req, res) => {
   res.json("Welcome to API Server.");
 });
+app.get("/db", (req, res) => {
+  const db = req.app.get("db");
+  // First create and seed the tables.
+  db.schema.hasTable("gfg_fbclone_users").then(exists => {
+    if (exists) {
+      // Table exists, do nothing.
+      console.log("Table Exists");
+      res.status(204).json();
+    } else {
+      // Table not there, let's create it.
+      console.log("Table Doesn't Exist");
+      // Create the table.
+      db.schema
+        .createTable("gfg_fbclone_users", table => {
+          table.increments("userid");
+          table.string("username");
+          table.string("password");
+          table.string("fullname");
+          table.timestamp("created_at").defaultTo(db.fn.now());
+        })
+        .then(() => {
+          res.status(201).json("Created the Users table.");
+        });
+    }
+  });
+});
 app.use("/users", users);
 
 // Export the app.
