40bd2000776efc99d71e2bec42002f46a16796da
Praveen Kumar <praveenscience@gmail.com>
2021-11-14 09:40:24 +0000

    Rendered form data from InpKeys & FormData.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -9,37 +9,30 @@ const Login = () => {
   const InpMap = {
     username: {
       type: "text",
-      name: "username",
       placeholder: "Username"
     },
     password: {
       type: "password",
-      name: "password",
       placeholder: "Password"
     },
     confpass: {
       type: "password",
-      name: "confpass",
       placeholder: "Confirm Password"
     },
     fullname: {
       type: "text",
-      name: "fullname",
       placeholder: "Full Name"
     },
     location: {
       type: "text",
-      name: "location",
       placeholder: "Current Location"
     },
     email: {
       type: "email",
-      name: "email",
       placeholder: "Email Address"
     },
     avatar: {
       type: "text",
-      name: "avatar",
       placeholder: "Your Avatar URL"
     }
   };
@@ -69,66 +62,16 @@ const Login = () => {
       <h1>Facebook Clone</h1>
       <form className="login-form">
         <div className="login-form-content">
-          <label>
-            <input
-              type="text"
-              name="username"
-              required={true}
-              placeholder="Username"
-            />
-          </label>
-          <label>
-            <input
-              type="password"
-              name="password"
-              required={true}
-              placeholder="Password"
-            />
-          </label>
-          {Reg ? (
-            <>
-              <label>
-                <input
-                  type="password"
-                  name="confpass"
-                  required={true}
-                  placeholder="Confirm Password"
-                />
-              </label>
-              <label>
-                <input
-                  type="text"
-                  name="fullname"
-                  required={true}
-                  placeholder="Full Name"
-                />
-              </label>
-              <label>
-                <input
-                  type="text"
-                  name="location"
-                  required={true}
-                  placeholder="Current Location"
-                />
-              </label>
-              <label>
-                <input
-                  type="email"
-                  name="email"
-                  required={true}
-                  placeholder="Email Address"
-                />
-              </label>
-              <label>
-                <input
-                  type="text"
-                  name="avatar"
-                  required={true}
-                  placeholder="Your Avatar URL"
-                />
-              </label>
-            </>
-          ) : null}
+          {Object.keys(FormData).map(fd => (
+            <label key={fd}>
+              <input
+                type={InpMap[fd].type}
+                name={fd}
+                required={true}
+                placeholder={InpMap[fd].placeholder}
+              />
+            </label>
+          ))}
         </div>
         <input type="submit" value={Reg ? "Register" : "Login"} />
       </form>
