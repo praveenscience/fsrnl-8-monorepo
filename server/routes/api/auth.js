7c2f24ceffce20c521950bacd7c6a1e96e11f21f
Praveen Kumar <praveenscience@gmail.com>
2021-11-14 07:07:53 +0000

    Auth Endpoint takes care of the following.
    * Login to the App.
    * Register with the App.
    * Get Current LoggedIn User.
    * Logout the Session.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -3,5 +3,10 @@ const express = require("express");
 // Create an instance of router.
 const app = express.Router();
 
+// Login to the App.
+// Register with the App.
+// Get Current LoggedIn User.
+// Logout the Session.
+
 // Export the app.
 module.exports = app;
