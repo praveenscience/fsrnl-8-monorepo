d2cc68a411c4e531d7f26efadcdebc69821bfe70
Praveen Kumar <praveenscience@gmail.com>
2021-11-14 06:52:36 +0000

    Created a LDS Ripple Component.

---
+++ client/src/components/LDSRipple.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LDSRipple = () => {
+  return (
+    <div className="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LDSRipple;
